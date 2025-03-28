20d0b631ab06e9ba2a0d9894a8f5a3e0b246deda
ParkSY0919 <qhr498@gmail.com>
2025-03-28 21:49:54 +0900

    [Setting/#1] 프로젝트 생성

---
+++ SeongjiChelin-iOS/SeongjiChelin-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeongjiChelin-iOS
+//
+//  Created by 박신영 on 3/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
