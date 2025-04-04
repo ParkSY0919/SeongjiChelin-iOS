c32e0688dc7f0d900ac9b91afba73379b1d3964f
ParkSY0919 <qhr498@gmail.com>
2025-04-04 13:31:07 +0900

    [Add/#5] UIImage 추가

--- SeongjiChelin-iOS/SeongjiChelin-iOS/Extension/UIImage+.swift
+++ SeongjiChelin-iOS/SeongjiChelin-iOS/Extension/UIImage+.swift
@@ -11,12 +11,42 @@ typealias SFConfig = UIImage.SymbolConfiguration
 
 extension UIImage {
     
-    static func plus(size: CGFloat = 16, color: UIColor = .gray) -> UIImage {
+    static func eyeglassesImage(size: CGFloat = 16, color: UIColor = .gray) -> UIImage {
         let config = SFConfig(pointSize: size)
-        let image = UIImage(systemName: "plus.app", withConfiguration: config)?
+        let image = UIImage(systemName: "eyeglasses", withConfiguration: config)?
+            .withRenderingMode(.alwaysTemplate)
+        return image!
+    }
+    
+    static func walkImage(size: CGFloat = 16) -> UIImage {
+        let config = SFConfig(pointSize: size)
+        let image = UIImage(systemName: "figure.walk", withConfiguration: config)?
+            .withRenderingMode(.alwaysTemplate)
+        return image!
+    }
+    
+    static func cartImage(size: CGFloat = 14, color: UIColor = .gray) -> UIImage {
+        let config = SFConfig(pointSize: size)
+        let image = UIImage(systemName: "cart", withConfiguration: config)?
+            .withRenderingMode(.alwaysTemplate)
+        return image!
+    }
+    
+    static func roadImage(size: CGFloat = 16, color: UIColor = .gray) -> UIImage {
+        let config = SFConfig(pointSize: size)
+        let image = UIImage(systemName: "road.lanes", withConfiguration: config)?
+            .withRenderingMode(.alwaysTemplate)
+        return image!
+    }
+    
+    static func person2Image(size: CGFloat = 12, color: UIColor = .gray) -> UIImage {
+        let config = SFConfig(pointSize: size)
+        let image = UIImage(systemName: "person.2.fill", withConfiguration: config)?
             .withRenderingMode(.alwaysTemplate)
         return image!
     }
 
 }
+
+
 
