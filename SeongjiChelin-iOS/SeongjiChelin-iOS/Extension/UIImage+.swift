e6cbe58caa064d008eb2b0da6cd8fdc41e048ceb
ParkSY0919 <qhr498@gmail.com>
2025-04-04 16:01:11 +0900

    [Feat/#5] SJStoreFilterButton 공용 컴포넌트 구현

--- SeongjiChelin-iOS/SeongjiChelin-iOS/Extension/UIImage+.swift
+++ SeongjiChelin-iOS/SeongjiChelin-iOS/Extension/UIImage+.swift
@@ -11,35 +11,35 @@ typealias SFConfig = UIImage.SymbolConfiguration
 
 extension UIImage {
     
-    static func eyeglassesImage(size: CGFloat = 16, color: UIColor = .gray) -> UIImage {
+    static func eyeglassesImage(size: CGFloat = 10, color: UIColor = .gray) -> UIImage {
         let config = SFConfig(pointSize: size)
         let image = UIImage(systemName: "eyeglasses", withConfiguration: config)?
             .withRenderingMode(.alwaysTemplate)
         return image!
     }
     
-    static func walkImage(size: CGFloat = 16) -> UIImage {
+    static func walkImage(size: CGFloat = 10) -> UIImage {
         let config = SFConfig(pointSize: size)
         let image = UIImage(systemName: "figure.walk", withConfiguration: config)?
             .withRenderingMode(.alwaysTemplate)
         return image!
     }
     
-    static func cartImage(size: CGFloat = 14, color: UIColor = .gray) -> UIImage {
+    static func cartImage(size: CGFloat = 10, color: UIColor = .gray) -> UIImage {
         let config = SFConfig(pointSize: size)
         let image = UIImage(systemName: "cart", withConfiguration: config)?
             .withRenderingMode(.alwaysTemplate)
         return image!
     }
     
-    static func roadImage(size: CGFloat = 16, color: UIColor = .gray) -> UIImage {
+    static func roadImage(size: CGFloat = 10, color: UIColor = .gray) -> UIImage {
         let config = SFConfig(pointSize: size)
         let image = UIImage(systemName: "road.lanes", withConfiguration: config)?
             .withRenderingMode(.alwaysTemplate)
         return image!
     }
     
-    static func person2Image(size: CGFloat = 12, color: UIColor = .gray) -> UIImage {
+    static func person2Image(size: CGFloat = 10, color: UIColor = .gray) -> UIImage {
         let config = SFConfig(pointSize: size)
         let image = UIImage(systemName: "person.2.fill", withConfiguration: config)?
             .withRenderingMode(.alwaysTemplate)
---
+++ SeongjiChelin-iOS/SeongjiChelin-iOS/Presentation/UIComponent/SJStoreFilterButton.swift
@@ -0,0 +1,90 @@
+//
+//  SJStoreFilterButton.swift
+//  SeongjiChelin-iOS
+//
+//  Created by 박신영 on 4/4/25.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class SJStoreFilterButton: UIButton {
+    
+    enum RestaurantTheme: String {
+        case sungSiKyungTheme = "성시경의 먹을텐데"
+        case ttoGanJibTheme = "풍자의 또간집"
+        case hongSeokCheonTheme = "홍석천과 이원일"
+        case baekJongWonTheme = "백종원의 님아 그 시장을 가오"
+    }
+    
+    private let image: UIImage
+    private let theme: RestaurantTheme
+    let tapSubject = PublishSubject<SJStoreFilterButton>()
+    
+    init(image: UIImage, theme: RestaurantTheme) {
+        self.image = image
+        self.theme = theme
+        
+        super.init(frame: .zero)
+        setSJStoreFilterButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setSJStoreFilterButton() {
+        var config = UIButton.Configuration.plain()
+        config.title = theme.rawValue
+        config.image = self.image
+        config.imagePadding = 4
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.seongiFont(.body_bold_12)
+            outgoing.foregroundColor = .text100
+            return outgoing
+        }
+        self.configuration = config
+        
+        let buttonStateHandler: UIButton.ConfigurationUpdateHandler = { button in
+            switch button.state {
+            case .normal:
+                button.configuration?.baseForegroundColor = .text100
+                button.configuration?.background.backgroundColor = .bg300
+            case .selected:
+                button.configuration?.baseForegroundColor = .bg100
+                button.configuration?.background.backgroundColor = .primary200
+            default:
+                return
+            }
+        }
+        self.configurationUpdateHandler = buttonStateHandler
+        
+        self.addTarget(
+            self,
+            action: #selector(SJStoreFilterButtonTapped),
+            for: .touchUpInside
+        )
+    }
+    
+    @objc
+    func SJStoreFilterButtonTapped() {
+        self.isUserInteractionEnabled = false
+        
+        self.isSelected.toggle()
+        switch self.isSelected {
+        case true:
+            tapSubject.onNext(self)
+        case false:
+            print(#function, "\(self.theme) 선택 취소")
+        }
+        
+        self.isUserInteractionEnabled = true
+    }
+    
+}
+
