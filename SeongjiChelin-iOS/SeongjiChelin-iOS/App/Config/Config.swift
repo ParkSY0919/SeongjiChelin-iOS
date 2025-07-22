5d088815932d701bf89579daf857aa9abd93ce6e
ParkSY0919 <qhr498@gmail.com>
2025-07-22 10:41:57 +0900

    [Setting/#28] add Config, .xcconfig in AppID

--- SeongjiChelin-iOS/SeongjiChelin-iOS/App/Config/Config.swift
+++ SeongjiChelin-iOS/SeongjiChelin-iOS/App/Config/Config.swift
@@ -11,6 +11,7 @@ enum Config {
     enum Keys {
         static let baseURL = "BASE_URL"
         static let googleAPIKey = "GOOGLE_API_KEY"
+        static let appID = "APPID"
     }
     
     private static let infoDictionary: [String: Any] = {
@@ -33,6 +34,13 @@ extension Config {
     
     static let googleAPIKey: String = {
         guard let key = Config.infoDictionary[Keys.googleAPIKey] as? String else {
+            fatalError("GOOGLE_API_KEY is not set in plist for this configuration")
+        }
+        return key
+    }()
+    
+    static let appID: String = {
+        guard let key = Config.infoDictionary[Keys.appID] as? String else {
             fatalError("GOOGLE_API_KEY is not set in plist for this configuration")
         }
         return key
